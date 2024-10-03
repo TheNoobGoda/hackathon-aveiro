32ecee1783a8364e6bbdbccaeaa0aa897d2d7864
TheNoobGoda <davidespinhamarques@gamil.com>
2024-10-03 14:38:36 +0100

    graph class finished

--- graph.py
+++ graph.py
@@ -40,39 +40,17 @@ class Graph:
             next_node = 0
             for edge in self.edges:
                     if edge.node1 == node and edge.node2 not in path:
-                        new_distance = distance + self.get_edge_lenght(node, edge.node2) + self.get_edge_lenght(edge.node2, node2)
+                        new_distance = distance + edge.distance + self.get_edge_lenght(edge.node2, node2)
                         if new_distance < distance2:
                             distance2 = distance + new_distance
                             next_node = edge.node2
 
                     elif edge.node2 == node and edge.bidirectional and edge.node1 not in path:
-                        new_distance = distance + self.get_edge_lenght(node, edge.node1) + self.get_edge_lenght(edge.node1, node2)
+                        new_distance = distance + edge.distance + self.get_edge_lenght(edge.node1, node2)
                         if new_distance < distance2:
                             distance2 = distance + new_distance
                             next_node = edge.node1
             path.append(next_node)
-            print(path)
             if next_node == node2: break
             
         return path
-
-
-    
-
-graph = Graph()
-
-for i in range(5):
-    graph.add_node(Node(random.random(),random.random()))
-
-
-graph.add_edge(Edge(0,4,graph.get_edge_lenght(0,4)))
-graph.add_edge(Edge(0,3,graph.get_edge_lenght(0,3)))
-graph.add_edge(Edge(1,2,graph.get_edge_lenght(1,2)))
-graph.add_edge(Edge(2,3,graph.get_edge_lenght(2,3)))
-graph.add_edge(Edge(0,1,graph.get_edge_lenght(0,1)))
-
-
-for edge in graph.edges:
-    print(edge.node1,edge.node2,edge.distance)
-
-print(graph.get_path(0,2))
