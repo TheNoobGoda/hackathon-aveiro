301ba2a302a122a1fba6e0611335cf844f0e286d
TheNoobSpine <davidespinhamarques@gmail.com>
2024-10-03 22:37:32 +0100

    added dijkstra

--- graph.py
+++ graph.py
@@ -1,8 +1,5 @@
 import mpu
-import random
-
-
-random.seed(42)
+import heapq
 
 class Node:
     def __init__(self,x,y):
@@ -27,30 +24,54 @@ class Graph:
     def add_edge(self,edge):
         self.edges.append(edge)
 
+    def get_edge_id(self,node1,node2):
+        i = 0
+        for edge in self.edges:
+            if (edge.node1 == node1 and edge.node2 == node2) or (edge.node1 == node2 and edge.node2 == node1): return i
+            else: i +=1
+
     def get_edge_lenght(self,node1,node2):
         return mpu.haversine_distance((self.nodes[node1].x,self.nodes[node1].y),(self.nodes[node2].x,self.nodes[node2].y))
     
-    def get_path(self,node1,node2):
-        path = [node1]
-        distance = 0
+    def get_neighbors(self, node):
+        """Get neighboring nodes and their distances from the current node."""
+        neighbors = []
+        for edge in self.edges:
+            if edge.node1 == node:
+                neighbors.append((edge.node2, edge.distance))
+            elif edge.bidirectional and edge.node2 == node:
+                neighbors.append((edge.node1, edge.distance))
+        return neighbors
 
-        while(True):
-            distance2 = float('inf')
-            node = path[len(path)-1]
-            next_node = 0
-            for edge in self.edges:
-                    if edge.node1 == node and edge.node2 not in path:
-                        new_distance = distance + edge.distance + self.get_edge_lenght(edge.node2, node2)
-                        if new_distance < distance2:
-                            distance2 = distance + new_distance
-                            next_node = edge.node2
-
-                    elif edge.node2 == node and edge.bidirectional and edge.node1 not in path:
-                        new_distance = distance + edge.distance + self.get_edge_lenght(edge.node1, node2)
-                        if new_distance < distance2:
-                            distance2 = distance + new_distance
-                            next_node = edge.node1
-            path.append(next_node)
-            if next_node == node2: break
+    def dijkstra(self, start, end):
+        """Implement Dijkstra's algorithm to find the shortest path from start to end."""
+        distances = {node: float('inf') for node in range(len(self.nodes))}
+        distances[start] = 0
+        priority_queue = [(0, start)]  # (distance, node)
+        previous_nodes = {node: None for node in range(len(self.nodes))}
+        
+        while priority_queue:
+            current_distance, current_node = heapq.heappop(priority_queue)
             
-        return path
+            if current_node == end:
+                break  # Found the shortest path to the target
+            
+            if current_distance > distances[current_node]:
+                continue
+            
+            for neighbor, edge_distance in self.get_neighbors(current_node):
+                distance = current_distance + edge_distance
+                if distance < distances[neighbor]:
+                    distances[neighbor] = distance
+                    previous_nodes[neighbor] = current_node
+                    heapq.heappush(priority_queue, (distance, neighbor))
+        
+        # Reconstruct the path
+        path = []
+        current = end
+        while current is not None:
+            path.append(current)
+            current = previous_nodes[current]
+        
+        path = path[::-1]  # Reverse the path to get it from start to end
+        return path if path[0] == start else None  # Return path or None if no path is found
