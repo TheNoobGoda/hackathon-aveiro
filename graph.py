fe78a08f8b967a5796a23fc4ca1223f2e21163a9
TheNoobGoda <davidespinhamarques@gamil.com>
2024-10-04 11:02:45 +0100

    added cars to dijkstra

--- graph.py
+++ graph.py
@@ -5,13 +5,13 @@ class Node:
     def __init__(self,x,y):
         self.x = x
         self.y = y
+        self.num_cars = 0
 
 class Edge:
     def __init__(self,node1,node2,distance,bidirectional = True):
         self.node1 = node1
         self.node2 = node2
         self.distance = distance
-        self.traffic_weight = 0 
         self.bidirectional = bidirectional
 
 class Graph:
@@ -35,16 +35,23 @@ class Graph:
         return mpu.haversine_distance((self.nodes[node1].x,self.nodes[node1].y),(self.nodes[node2].x,self.nodes[node2].y))
     
     def get_neighbors(self, node):
+        """
+        Get neighboring nodes and the total weight (distance + traffic_weight) for each edge.
+        """
         neighbors = []
         for edge in self.edges:
             if edge.node1 == node:
-                neighbors.append((edge.node2, edge.distance))
+                total_weight = edge.distance
+                neighbors.append((edge.node2, total_weight))
             elif edge.bidirectional and edge.node2 == node:
-                neighbors.append((edge.node1, edge.distance))
+                total_weight = edge.distance
+                neighbors.append((edge.node1, total_weight))
         return neighbors
 
     def dijkstra(self, start, end):
-        """Implement Dijkstra's algorithm to find the shortest path from start to end."""
+        """
+        Implement Dijkstra's algorithm considering both distance and traffic weight.
+        """
         distances = {node: float('inf') for node in range(len(self.nodes))}
         distances[start] = 0
         priority_queue = [(0, start)]  # (distance, node)
@@ -59,8 +66,8 @@ class Graph:
             if current_distance > distances[current_node]:
                 continue
             
-            for neighbor, edge_distance in self.get_neighbors(current_node):
-                distance = current_distance + edge_distance
+            for neighbor, edge_weight in self.get_neighbors(current_node):
+                distance = current_distance + edge_weight + self.nodes[neighbor].num_cars
                 if distance < distances[neighbor]:
                     distances[neighbor] = distance
                     previous_nodes[neighbor] = current_node
@@ -75,23 +82,19 @@ class Graph:
         
         path = path[::-1]  # Reverse the path to get it from start to end
         return path if path[0] == start else None  # Return path or None if no path is found
-    
-    def update_edge_weights(self, path):
-        """Increase the traffic weight for each edge along the given path."""
-        for i in range(len(path) - 1):
-            node1, node2 = path[i], path[i+1]
-            for edge in self.edges:
-                if (edge.node1 == node1 and edge.node2 == node2) or (edge.node1 == node2 and edge.node2 == node1):
-                    edge.traffic_weight += 1  # Increase weight due to traffic
-                    break
 
-    def find_closest_node(self, x, y):
-        """Find the closest node to a given GPS location."""
-        min_distance = float('inf')
-        closest_node = None
+
+    def find_closest_nodes(self, x, y):
+        """
+        Find the closest and second closest nodes to a given GPS location.
+        """
+        distances = []
         for i, node in enumerate(self.nodes):
             distance = mpu.haversine_distance((node.x, node.y), (x, y))
-            if distance < min_distance:
-                min_distance = distance
-                closest_node = i
+            distances.append((distance, i))
+        
+        # Sort by distance
+        distances.sort()
+
+        closest_node = distances[0][1]  # Closest node
         return closest_node
